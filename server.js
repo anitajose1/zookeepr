4f6497c2b21d0e18fad68eb1eac360cc2f9c7c48
Anita Jose <anitajose119@gmail.com>
2022-08-28 14:34:00 -0400

    allowed user creation of data:

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
+
 // Note: Heroku sets the environment variable PORT to 80
 const PORT = process.env.PORT || 3001
+
 const app = express()
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}))
+// parse incoming JSON data
+app.use(express.json())
+
 const {animals} = require('./data/animals.json')
+const { json } = require('body-parser')
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = []
@@ -47,6 +57,18 @@ function findById(id, animalsArray) {
     return result
 }
 
+// function to take  data from req.body and add it to our animals.json file
+function createNewAnimal(body, animalsArray) {
+    const animal = body
+    // our function's main code will go here!
+    animalsArray.push(animal)
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    )
+    return animal
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals
     if (req.query) {
@@ -64,6 +86,38 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404)
     }
 })
+
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    // set id value based on what the next index of the array will be
+    req.body.id = animals.length.toString()
+
+    // if any data in req.body is incorrect, send 400 error back
+    if(!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.')
+    } else {
+        // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals)
+    res.json(animal)
+    }
+})
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false
+    }
+    return true
+}
 
 // it's okay to place this method at the top instead of here as long as 'app' is declared first
 app.listen(PORT, () => {
